e95bdc4237188eef4b1238b0516e81e5314ab0bc
s-foran <samuel.dm.foran@gmail.com>
2025-11-23 15:52:18 -0700

    RS-001 Add more scraper RSpecs

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -10,5 +10,19 @@ RSpec.describe RubySpider::Scraper do
       result = RubySpider::Scraper.scrape("https://example.com", "h1")
       expect(result).to eq("Hello World")
     end
+
+    it "returns nil if the CSS selector is not found" do
+      stub_request(:get, "https://example.com")
+        .to_return(status: 200, body: "<html><body><h1> Hello World </h1><p> This is a test. </p></body></html>")
+      result = RubySpider::Scraper.scrape("https://example.com", "h2")
+      expect(result).to eq(nil)
+    end
+
+    it "returns nil for an invalid CSS selector" do
+      stub_request(:get, "https://example.com")
+        .to_return(status: 200, body: "<html><body><h1> Hello World </h1><p> This is a test. </p></body></html>")
+      result = RubySpider::Scraper.scrape("https://example.com", "N/A")
+      expect(result).to eq(nil)
+    end
   end
 end
